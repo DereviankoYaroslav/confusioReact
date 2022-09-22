a995e8a900e876367beb5d15d20c8075adab0efe
DereviankoYaroslav <yarik0009258@gmail.com>
2022-09-22 16:16:25 +0300

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Comment from './CommentForm';
+import { Loading } from './LoadingComponent';
 
 
 
@@ -43,7 +44,25 @@ import Comment from './CommentForm';
 
     const DishDetail = (props) => {
 
-        if (props != null){
+        if(props.isLoading){
+            return(
+                <div className='container'>
+                    <div className='row'>
+                        <Loading />
+                    </div>
+                </div>
+            );
+        } else if (props.errMess){
+            return(
+                <div className='container'>
+                    <div className='row'>
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+
+        else if (props != null){
             return (
                 <div className="container">
                     <div className="row">
